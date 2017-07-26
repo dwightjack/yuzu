9c0df04fed16797bb94b5516fa514f75e0fb5069
Marco Solazzi <marco.solazzi+git@aquest.it>
2017-07-26 13:02:09 +0200

    added utility functions

--- src/utils.js
+++ src/utils.js
@@ -4,3 +4,31 @@ export const UID_PREFIX: string = '_ui.';
 
 let uid: number = -1;
 export const nextUid = (prefix: string = UID_PREFIX): string => prefix + (++uid); //eslint-disable-line no-plusplus
+
+
+
+export const isObjectLike = (value: any): boolean => !!value && typeof value === 'object';
+
+
+
+const funcToString = Function.prototype.toString;
+const objProto = Object.prototype;
+const objToString = objProto.toString;
+const hasOwnProperty = objProto.hasOwnProperty;
+const objectCtorString = funcToString.call(Object);
+
+export const isPlainObject = (value: any): boolean => {
+    if (!isObjectLike(value) || objToString.call(value) !== '[object Object]') {
+        return false;
+    }
+    const proto = Object.getPrototypeOf(value);
+    if (proto === null) {
+        return true;
+    }
+    const Ctor = hasOwnProperty.call(proto, 'constructor') && proto.constructor;
+    return (typeof Ctor === 'function' && Ctor instanceof Ctor && funcToString.call(Ctor) === objectCtorString);
+};
+
+export const isElement = (value: any): boolean => (
+    !!value && value.nodeType === 1 && isObjectLike(value) && !isPlainObject(value)
+);
