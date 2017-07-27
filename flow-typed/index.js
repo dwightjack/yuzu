f8fb5bb8a01f0c71d93f531796b7128436d0edd5
Marco Solazzi <marco.solazzi+git@aquest.it>
2017-07-27 17:48:06 +0200

    linting

--- flow-typed/index.js
+++ flow-typed/index.js
@@ -1,8 +1,8 @@
 // @flow
 
-export type optionsType = {[option_key: string]: any}
+export type optionsType = {[option_key: string]: any};
 
-export type stateType = {[state_key: string]: any}
+export type stateType = {[state_key: string]: any};
 
 export type refInstanceType = {|
     component: Component,
