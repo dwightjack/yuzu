a170985dff8c3f5ce405aed7abe2291455acf864
Marco Solazzi <marco.solazzi+git@aquest.it>
2017-08-17 14:20:12 +0200

    definitions

--- flow-typed/index.js
+++ flow-typed/index.js
@@ -4,6 +4,10 @@ export type optionsType = {[option_key: string]: any};
 
 export type stateType = {[state_key: string]: any};
 
+export type RootElement = Element | string;
+
+export type childIterator = (el: Element, index: number) => any
+
 export type refInstanceType = {|
     component: Component,
     id: string,
@@ -13,7 +17,7 @@ export type refInstanceType = {|
 export type refConstructorType = {|
     component: Class<Component>,
     id: string,
-    el: Element | string,
+    el: RootElement,
     opts?: optionsType,
     props?: {}
 |};
