a9982d77ae6a12de376ec417adb07698b7162e05
Marco Solazzi <marco.solazzi+git@aquest.it>
2017-08-29 17:51:29 +0200

    docs

--- src/mount.js
+++ src/mount.js
@@ -3,6 +3,83 @@ import { qs } from 'tsumami';
 
 import type Component from './component';
 
+/**
+ * Returns a mount function which in turn accepts a state object and a parent component.
+ *
+ * This function is useful to setup trees of components in a functional way
+ *
+ * It accepts 4 arguments:
+ *
+ * * a component constructor (either created by extending `Component` or by `Component.create`)
+ * * a mount DOM node (either a CSS selector string or a DOM element)
+ * * component options _(optional)_
+ * * An optional array of children mount functions OR a function returning an array of children mount functions (usually yuzu's `Children` function)
+ *
+ * Instances of components passed as children will be set a parent's components references (uses: [`Component#setRef`](./component.md#setRef))
+ * ### A simple, single component example:
+ *
+ * ```js
+ * import { mount, Component } from 'yuzu';
+ *
+ * import GalleryComponent from './components/Gallery';
+ *
+ * const tree = mount(
+ *  GalleryComponent,
+ *  '#gallery', //mount point,
+ *  { theme: 'red' } //options
+ * );
+ *
+ * //attach the tree with an initial state passed to the root component
+ * const gallery = tree({ startIndex: 1 })
+ * ```
+ *
+ * ### A components' tree example
+ *
+ * Props can be passed to children components by setting a `prop` property on the `options` object
+ *
+ * ```js
+ * import { mount, Component } from 'yuzu';
+ *
+ * import List from './components/List';
+ * import ListItem from './components/ListItem';
+ *
+ * const tree = mount(
+ *  List,
+ *  '#list', //mount point,
+ *  null //empty options
+ *  [
+ *      mount(ListItem, '.list-item1', { props: { currentItem: 'current'} }),
+ *      mount(ListItem, '.list-item2', { props: { currentItem: 'current'} })
+ *  ]
+ * );
+ *
+ * //attach the tree with an initial state passed to the root component
+ * const list = tree({ currentItem: 0 })
+ * ```
+ *
+ * ### A dynamic components' tree example
+ *
+ * Props can be passed to children components by setting a `prop` property on the `options` object
+ *
+ * ```js
+ * import { mount, Component, Children } from 'yuzu';
+ *
+ * import List from './components/List';
+ * import ListItem from './components/ListItem';
+ *
+ * const tree = mount(
+ *  List,
+ *  '#list', //mount point,
+ *  null //empty options
+ *  Children('.list-item', (el, id) => {
+ *      return mount(ListItem, el, { id: `list-item-${i}`, props: { currentItem: 'current'} });
+ *  })
+ * );
+ *
+ * //attach the tree with an initial state passed to the root component
+ * const list = tree({ currentItem: 0 })
+ * ```
+ */
 const mount = function mount(
     ComponentConstructor: Class<Component>,
     el: Element | string,
