959f2be2820021d5f492102b1ae56a22ced59691
Marco Solazzi <marco.solazzi@gmail.com>
2018-08-17 17:25:46 +0900

    feat(component): create
    re-introduced an ES5 compatible method to create components

---
+++ src/create.ts
@@ -0,0 +1,29 @@
+import { Component } from './component';
+
+export const create = (
+  props: { [key: string]: any } = {},
+  parent = Component,
+) => {
+  const child = props.hasOwnProperty('constructor')
+    ? props.constructor
+    : function ChildConstructor(...args: any[]) {
+        return parent.apply(parent, ...args);
+      };
+
+  // https://github.com/mridgway/hoist-non-react-statics/blob/master/index.js#L51
+  const keys = Object.getOwnPropertyNames(parent);
+  for (const key of keys) {
+    const descriptor = Object.getOwnPropertyDescriptor(parent, key);
+    try {
+      // Avoid failures from read-only properties
+      Object.defineProperty(child, key, descriptor as PropertyDescriptor);
+    } catch (e) {} // tslint:disable-line no-empty
+  }
+
+  child.prototype = Object.assign(Object.create(parent.prototype), props);
+  child.prototype.constructor = child;
+
+  (child as any).__super__ = parent.prototype;
+
+  return child;
+};
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { Children } from './children';
 export { mount } from './mount';
 export { Sandbox } from './sandbox';
 export { Loadable } from './loadable';
+export { create } from './create';
