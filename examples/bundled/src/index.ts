fae12c47501177c0149cc386ecc4309d71c50445
Marco Solazzi <marco.solazzi@gmail.com>
2018-10-15 19:44:41 +0900

    demo build

---
+++ examples/bundled/src/index.ts
@@ -0,0 +1,36 @@
+import { Component, devtools } from '../../../packages/yuzu/src';
+import { Output } from './output';
+
+devtools(Component);
+
+class App extends Component {
+  public selectors = {
+    pre: 'pre',
+  };
+
+  public state = {
+    output: [],
+  };
+
+  public actions = {
+    output(v) {
+      this.$els.pre.innerText = JSON.stringify(v, null, 2);
+    },
+  };
+
+  public initialize() {
+    this.setRef({
+      id: 'output',
+      component: Output,
+      on: {
+        append: (value) => {
+          this.setState(({ output }) => ({
+            output: [...output, value],
+          }));
+        },
+      },
+    });
+  }
+}
+
+new App().mount('#app');
