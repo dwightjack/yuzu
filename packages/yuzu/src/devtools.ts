6a6d89567bbe890b09b9ad5a4ca95edc88d4a081
Marco Solazzi <marco.solazzi@gmail.com>
2018-10-15 19:44:08 +0900

    show component tree in devtools

--- packages/yuzu/src/devtools.ts
+++ packages/yuzu/src/devtools.ts
@@ -1,5 +1,5 @@
 import { Component } from './component';
-
+import { IObject } from '../types';
 /**
  * `devtools` is an helper function that will expose the instance of a Component in a `$yuzu` property attached to its root DOM element.
  *
@@ -23,12 +23,38 @@ if (process.env.NODE_ENV !== 'production') {
     const proto = ComponentClass.prototype;
     const { mount } = proto;
 
+    function refTree(root: Component, tree: IObject) {
+      if (root.$refsStore.size > 0) {
+        root.$refsStore.forEach((ref, name) => {
+          tree[name] = Object.create(null);
+          refTree(ref, tree[name]);
+        });
+      }
+      Object.defineProperty(tree, '$self', {
+        enumerable: false,
+        value: Object.assign(Object.create(null), {
+          $raw: root,
+          state: Object.assign(Object.create(null), root.state),
+        }),
+      });
+      return tree;
+    }
+
     proto.mount = function mountDev(...args) {
       mount.call(this, ...args);
       Object.defineProperty(this.$el as YuzuRoot, '$yuzu', {
         enumerable: false,
         writable: false,
         value: this,
+      });
+
+      Object.defineProperty(this, '$$getTree', {
+        enumerable: false,
+        writable: false,
+        value() {
+          const tree = Object.create(null);
+          return refTree(this, tree);
+        },
       });
       return this;
     };
