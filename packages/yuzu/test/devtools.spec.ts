f5182fd038eac0fe7879734e0f97e11b6b2950f8
Marco Solazzi <marco.solazzi@gmail.com>
2018-12-06 15:59:34 +0900

    tests

--- packages/yuzu/test/devtools.spec.ts
+++ packages/yuzu/test/devtools.spec.ts
@@ -1,30 +1,101 @@
-import { mount as mountHTML } from '../../../shared/utils';
 import { Component } from '../src/component';
 import { devtools, YuzuRoot } from '../src/devtools';
-import * as utils from 'yuzu-utils';
+import { fn, IStateLogger } from '../types';
+
+type ILogger = IStateLogger<Component>;
 
 describe('`devtools`', () => {
   let el: HTMLElement;
   let inst: Component;
   let mount: any;
+  let init: any;
 
   class Child extends Component {}
 
   beforeAll(() => {
     mount = Component.prototype.mount;
+    init = Component.prototype.init;
     devtools(Component);
   });
 
   beforeEach(() => {
     el = document.createElement('div');
-    inst = new Child().mount(el);
+    inst = new Child().mount(el, null);
   });
 
   it('attaches a $yuzu property at mount', () => {
     expect((inst.$el as YuzuRoot).$yuzu).toBe(inst);
   });
 
+  describe('$$logStart', () => {
+    it('attaches a $$logStart property at init', () => {
+      inst.init();
+      expect(inst.$$logStart).toEqual(jasmine.any(Function));
+    });
+
+    it('initilizes a $$logger property', () => {
+      inst.init();
+      (inst.$$logStart as fn)('LABEL');
+      expect(inst.$$logger).not.toBeUndefined();
+      expect((inst.$$logger as ILogger).label).toBe('LABEL');
+    });
+
+    it('subscribes to the "change:*" state event', () => {
+      inst.init();
+      (inst.$$logStart as fn)('LABEL', false);
+
+      const spy = spyOn(inst.$$logger as ILogger, 'subscribe');
+
+      (inst.$$logStart as fn)('LABEL');
+      expect(spy).toHaveBeenCalledWith(inst, 'change:*');
+
+      (inst.$$logStart as fn)('LABEL', 'change:my');
+      expect(spy).toHaveBeenCalledWith(inst, 'change:my');
+    });
+
+    it('does NOT execute subscribe if the second argment is "false"', () => {
+      inst.init();
+      (inst.$$logStart as fn)('LABEL', false);
+
+      const spy = spyOn(inst.$$logger as ILogger, 'subscribe');
+
+      (inst.$$logStart as fn)('LABEL', false);
+      expect(spy).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('$$logStart', () => {
+    it('attaches a $$logEnd property at init', () => {
+      inst.init();
+      expect(inst.$$logEnd).toEqual(jasmine.any(Function));
+    });
+
+    it('does NOT break if called before $$logStart', () => {
+      inst.init();
+      expect(() => {
+        (inst.$$logEnd as fn)();
+      }).not.toThrow();
+    });
+
+    it('calls unsubscribe when an event is specified', () => {
+      inst.init();
+      (inst.$$logStart as fn)('LABEL', false);
+      const spy = spyOn(inst.$$logger as ILogger, 'unsubscribe');
+      (inst.$$logEnd as fn)('EVENT');
+      expect(spy).toHaveBeenCalledWith(inst, 'EVENT');
+    });
+
+    it('calls unsubscribeAll when an event is NOT specified', () => {
+      inst.init();
+      (inst.$$logStart as fn)('LABEL', false);
+      const spy = spyOn(inst.$$logger as ILogger, 'unsubscribeAll');
+      (inst.$$logEnd as fn)();
+      expect(spy).toHaveBeenCalledWith(inst);
+    });
+  });
+
   afterAll(() => {
     Component.prototype.mount = mount;
+    Component.prototype.init = init;
   });
 });
